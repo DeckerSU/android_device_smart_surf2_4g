d6c8a73ca15ee11f11a5a7299fff1fbb7292c68a
DeckerSU <support@decker.su>
2016-12-12 21:05:11 +0200

    fix access wvm to ReadOptions (patch)

--- patches_decker/check-patches.sh
+++ patches_decker/check-patches.sh
@@ -2,5 +2,6 @@
 cd ../../../..
 cd frameworks/av
 git apply -v --check ../../device/mts/smart_surf2_4g/patches_decker/0001-mtk-audio-fix.patch
+git apply -v --check ../../device/mts/smart_surf2_4g/patches_decker/0002-fix-access-wvm-to-ReadOptions.patch
 cd ../..
 echo Patches Applied Successfully!
